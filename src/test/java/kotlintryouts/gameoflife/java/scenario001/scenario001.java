f105c5aabeaa466ef1828b551b9d0ac75fd942d6
marcuslang <marcuslang@zoho.com>
2016-02-16 21:19:52 +0100

    - scenario003 for java&kotlin

--- src/test/java/kotlintryouts/gameoflife/java/scenario001/scenario001.java
+++ src/test/java/kotlintryouts/gameoflife/java/scenario001/scenario001.java
@@ -1,40 +1,40 @@
-package kotlintryouts.gameoflife.kotlin.scenario001;
+package kotlintryouts.gameoflife.java.scenario001;
 
-import kotlintryouts.gameoflife.kotlin.KotlinGameOfLife;
+import kotlintryouts.gameoflife.java.GameOfLife;
 import org.junit.Before;
 import org.junit.Test;
 
 public class Scenario001 {
 
-    private KotlinGameOfLife kgof;
+    private GameOfLife gof;
     private final static int XSIZE = 150;
     private final static int YSIZE = 100;
     private final static int GENERATIONS = 7;
 
     @Before
     public void setUp() throws Exception {
-        kgof = new KotlinGameOfLife(XSIZE, YSIZE);
-        kgof.setPointAlive(49, 50);
-        kgof.setPointAlive(50, 50);
-        kgof.setPointAlive(51, 50);
+        gof = new GameOfLife(XSIZE, YSIZE);
+        gof.setPointAlive(49, 50);
+        gof.setPointAlive(50, 50);
+        gof.setPointAlive(51, 50);
 
-        kgof.setPointAlive(50, 51);
+        gof.setPointAlive(50, 51);
 
-        kgof.setPointAlive(49, 52);
-        kgof.setPointAlive(50, 52);
-        kgof.setPointAlive(51, 52);
+        gof.setPointAlive(49, 52);
+        gof.setPointAlive(50, 52);
+        gof.setPointAlive(51, 52);
     }
 
     @Test
     public void dieAfterSevenGenerations() throws Exception {
         System.out.println("Start:");
-        print(kgof.getGrid());
+        print(gof.getGrid());
         System.out.println();
 
         for (int i = 1; i <= GENERATIONS; i++) {
-            kgof.doGeneration();
+            gof.doGeneration();
             System.out.println("Generation = " + i);
-            print(kgof.getGrid());
+            print(gof.getGrid());
             System.out.println();
         }
 
